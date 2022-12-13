1cf72a7f6a2178ead001b3de013f8a6479f63fd9
Metastem <110768529+Metastem@users.noreply.github.com>
2022-12-13 20:10:14 +0800

    Create list3.jsx

---
+++ src/assets/lists/list3.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class List3 extends React.Component {
+  render() {
+    return(
+      <>
+        <ul class=" text-left text-off-white1 m-0 pl-5 pt-1 before:block before:absolute left-0 w-1 h-3 border-solid border-teal-600 relative list-none mb-2">
+            <li class="font-black text-lg underline underline-offset-4 decoration-2 decoration-off-white1 text-center">Privacy Matters</li>
+            <li class=""><a href="https://hckrnws.metastem.su/">HackerNews</a></li>
+            <li class=""><a href="https://chitchatter.metastem.su/">ChitChatter</a></li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default List3" > src/assets/lists/list3.jsx
