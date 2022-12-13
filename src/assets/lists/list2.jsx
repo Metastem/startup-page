0cb6ced528f7423f02fd30563e80fc6b3d3d0d87
Metastem <110768529+Metastem@users.noreply.github.com>
2022-12-13 20:07:31 +0800

    Create list2.jsx

---
+++ src/assets/lists/list2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class List2 extends React.Component {
+  render() {
+    return(
+      <>
+        <ul class=" text-left text-off-white1 m-0 pl-5 pt-1 before:block before:absolute left-0 w-1 h-3 border-solid border-teal-600 relative list-none mb-2">
+            <li class="font-black text-lg underline underline-offset-4 decoration-2 decoration-off-white1 text-center">Fun</li>
+            <li class=""><a href="https://anime.metastem.su/">Anime</a></li>
+            <li class=""><a href="https://play.metastem.su/">PlayVideo</a></li>
+            <li class=""><a href="https://troddit.metastem.su/">Troddit</a></li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default List2" > src/assets/lists/list2.jsx
